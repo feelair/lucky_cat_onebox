5adcc00662a55fb75b382a040da855add114023c
cammsia <cammsia@gmail.com>
2019-08-03 23:00:58 +0800

    bug-fix

--- plugin.rb
+++ plugin.rb
@@ -2,9 +2,9 @@
 # about: 为 Discourse 增加了各种本地化的功能。
 # version: 2.0.0
 # authors: Erick Guan
-# url: https://github.com/fantasticfears/discourse-chinese-localization-pack
 
-enabled_site_setting :zh_l10n_enabled
+
+enabled_site_setting :lucky_cat_onebox
 
 require 'active_support/inflector'
 require "ostruct"
@@ -17,7 +17,7 @@ Dir[File.expand_path('../lib/onebox_override/*.rb', __FILE__)].each { |f| requir
 register_asset "stylesheets/buttons.scss"
 
 after_initialize do
-  next unless SiteSetting.zh_l10n_enabled
+  next unless SiteSetting.lucky_cat_onebox
 
   Dir[File.expand_path('../lib/onebox/*.rb', __FILE__)].each { |f| require f }
 end
