5da5587ce5f0139873978ae5d2dd63d354dc34eb
cammsia <cammsia@gmail.com>
2019-08-03 23:58:34 +0800

    bug-fix

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,6 @@
-# name: Discourse 中文本地化服务集合
-# about: 为 Discourse 增加了各种本地化的功能。
-# version: 2.0.0
-# authors: Erick Guan
+# name: lucky-cat-inbox
+# version:0.0.1
+# authors: luckycat
 
 
 enabled_site_setting :lucky_cat_onebox_enable
